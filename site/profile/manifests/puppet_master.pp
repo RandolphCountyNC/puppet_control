ee1da6ed4556354a7a4935de8f72ce1d59d2659e
dgshue <daniel.shue@randolphcountync.gov>
2018-02-27 11:27:38 -0500

    Hiera v5 config

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -1,12 +1,13 @@
 class profile::puppet_master{
   class { 'hiera':
-    hierarchy => [
-      '%{fqdn}',
-      '%{osfamily}',
-      '%{environment}',
-      'common',
-      'accounts',
-      ],
-    eyaml     =>  true,
+    hiera_version   =>  '5',
+    hiera5_defaults =>  {'datadir' => 'data', 'data_hash' => 'yaml_data'},
+    hierarchy       =>  [
+                          {'name' =>  'Virtual yaml', 'path'  =>  'virtual/%{::virtual}.eyaml'},
+                          {'name' =>  'Nodes yaml', 'paths' =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.eyaml']},
+                          {'name' =>  'Default yaml file', 'path' =>  'common.eyaml'},
+                          {'name' =>  'Accounts', 'path' =>  'accounts.eyaml'},
+                        ],
+    eyaml           =>  true,
   }
 }
