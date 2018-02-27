943fd759066123c4e2a51dbb4ff065e465648d8f
dgshue <daniel.shue@randolphcountync.gov>
2018-02-27 10:34:49 -0500

    First user creation attempt

--- site/users/manifests/dmz_users.pp
+++ site/users/manifests/dmz_users.pp
@@ -1,9 +1,3 @@
 class users::dmz_users {
-  user {'itdgs':
-      ensure     => present,
-      name       => 'itdgs',
-      groups     => ['Users','Administrators'],
-      password   => 'plaintext_I_know_gross-use-hiera-or-blackbox',
-      managehome => true,
-  }
+  create_resources(user, hiera('dmz_users'))
 }
