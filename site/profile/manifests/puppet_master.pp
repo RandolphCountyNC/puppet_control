8efb057bc68b982a884d8cd23e149b5c6d36e0dd
dgshue <daniel.shue@randolphcountync.gov>
2018-02-27 12:14:47 -0500

    eyaml fix

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -3,12 +3,12 @@ class profile::puppet_master{
     hiera_version   =>  '5',
     hiera5_defaults =>  {'datadir' => '/etc/puppetlabs/code/environments/%{::environment}/hieradata', 'data_hash' => 'yaml_data'},
     hierarchy       =>  [
-                          {'name'        => 'Nodes yaml', 'paths' => ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
-                          {'name'        => 'Default yaml file', 'path' => 'common.yaml'},
-                          {'name'        => 'Accounts',
-                            'path'       => 'accounts.eyaml',
-                            'lookup_key' => 'eyaml_lookup_key',
-                            'options'    => {
+                          {'name'       => 'Nodes yaml', 'paths' => ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
+                          {'name'       => 'Default yaml file', 'path' => 'common.yaml'},
+                          {'name'       => 'Accounts',
+                          'path'        => 'accounts.eyaml',
+                          'lookup_key'  => 'eyaml_lookup_key',
+                          'options'     => {
                               'pkcs7_private_key' => '/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem',
                               'pkcs7_public_key'  => '/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem'
                               },
