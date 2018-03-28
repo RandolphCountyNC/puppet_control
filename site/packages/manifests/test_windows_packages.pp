1c9fa062c64df14ea49685d3e43dbc5a6db15a36
dgshue <daniel.shue@randolphcountync.gov>
2018-03-28 09:31:45 -0400

    Fix

--- site/packages/manifests/test_windows_packages.pp
+++ site/packages/manifests/test_windows_packages.pp
@@ -1,14 +1,14 @@
 class packages::test_windows_packages {
   file { 'WindowsAgentRandolph_x64.msi' :
     ensure => 'file',
-    path   => 'C:\\Ziptemp\\WindowsAgentRandolph_x64.msi',
-    source => 'puppet:///modules/scripts/files/WindowsAgentRandolph_x64.msi',
+    path   => 'C:/Ziptemp/WindowsAgentRandolph_x64.msi',
+    source => 'puppet:///modules/msi/WindowsAgentRandolph_x64.msi',
     mode   => '0774',
   }
   package { 'traps' :
     ensure   => present,
     provider => 'windows',
-    source   => 'C:\\Ziptemp\\WindowsAgentRandolph_x64.msi',
+    source   => 'C:/Ziptemp/WindowsAgentRandolph_x64.msi',
     require  => File['WindowsAgentRandolph_x64.msi'],
   }
 }
